90665d6c06a516a4b6afcf1c2350ca37f70dcc0c
diegorizzo <diegochunr@hotmail.com>
2026-03-19 20:18:18 -0600

    Sección C Terminada

--- mi-perfil/app.js
+++ mi-perfil/app.js
@@ -6,8 +6,18 @@ const mostrarEtiquetas = (habilidades) => {
 }
 
 const createSpan = () => {
-    const etiquetas = document.querySelector("#etiquetas")
+    const etiquetas = document.querySelector("#etiquetas");
     let nuevoSpan = document.createElement("span");
     nuevoSpan.classList.add("etiqueta");
-    etiquetas.appendChild(nuevoSpan)
+    etiquetas.appendChild(nuevoSpan);
+}
+
+const construirPerfil = (datos) => {
+    return {
+        nombre: datos.name,
+        usuario: "@" + datos.login,
+        email: datos.email || "No disponible",
+        ciudad: datos.location || "Sin ubicación",
+        avatar: datos.avatar_url,
+    }
 }
